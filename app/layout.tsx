f2b3692e282fd46aa92d9790587d16454f38ad90
Shivam Roy <royshivam0123@gmail.com>
2025-10-11 10:41:04 +0530

    Revert "Updated Pointer and Background"
    This reverts commit bd3286be62b15fca8cd2d152b16721be637ef57a.
    
            Update to previous commit 0e8be65010ef

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import Footer from "@/components/Footer";
-import Mousepointer from "@/components/Mousepointer";
-import StarsBackground from "@/components/Starsbackground"
+
 
 export const metadata: Metadata = {
   title: "Shivam Roy",
@@ -13,22 +12,18 @@ export const metadata: Metadata = {
 export default function DashboardLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-screen bg-white dark:bg-gray-900 relative overflow-hidden">
-        <StarsBackground />
+      <body className="flex flex-col min-h-screen bg-white dark:bg-gray-900">
+        <Navbar />
 
-        <Mousepointer>
-          <Navbar />
+        {/* Main content area grows to push footer down */}
+        <main className="flex-grow">{children}</main>
 
-          {/* Main content area grows to push footer down */}
-          <main className="flex-grow">{children}</main>
-
-          <Footer />
-        </Mousepointer>
+        <Footer />
       </body>
     </html>
-  );
+  )
 }
