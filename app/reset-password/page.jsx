dccaf09dfb3ed627fbe662b3fe0b47430921c344
Shivam Roy <royshivam0123@gmail.com>
2025-12-18 21:57:50 +0530

    Update reset-password

--- app/reset-password/page.jsx
+++ app/reset-password/page.jsx
@@ -1,7 +1,10 @@
 import { Suspense } from "react";
 import ResetPasswordClient from "./ResetPasswordClient";
 
+export const dynamic = "force-dynamic";
+
 export default function ResetPasswordPage() {
+  
   return (
     <Suspense fallback={<p className="text-center mt-10">Loading...</p>}>
       <ResetPasswordClient />
